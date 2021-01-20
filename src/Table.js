f9f006efa5f6ba501f3ea4241a78f78f7be4e1aa
Alan <allen0lee@gmail.com>
2021-01-20 12:48:29 +1100

    create table component

--- src/Table.js
+++ src/Table.js
@@ -1,48 +1,52 @@
-import React from "react";
-import { useTable } from "react-table";
+import React from 'react'
 
-export default function Table({ columns, data }) {
-  // Use the useTable Hook to send the columns and data to build the table
-  const {
-    getTableProps, // table props from react-table
-    getTableBodyProps, // table body props from react-table
-    headerGroups, // headerGroups, if your table has groupings
-    rows, // rows for the table based on the data passed
-    prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
-  } = useTable({
-    columns,
-    data
-  });
+export default class Table extends React.Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        students: [
+           { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
+           { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
+           { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
+           { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
+        ]
+     }
+  }
 
-  /* 
-    Render the UI for your table
-    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
-  */
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
+  renderTableHeader() {
+     let header = Object.keys(this.state.students[0])
+     return header.map((key, index) => {
+        return <th key={index}>{key.toUpperCase()}</th>
+     })
+  }
 
-          // Each row can be rendered directly as a string using the react-table render method
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  renderTableData() {
+     return this.state.students.map((student, index) => {
+        const { id, name, age, email } = student //destructuring
+        return (
+           <tr key={id}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{age}</td>
+              <td>{email}</td>
+           </tr>
+        )
+     })
+  }
+
+  render() {
+     return (
+        <div>
+           <h1 id='title'>React Dynamic Table</h1>
+           <table id='students'>
+              <tbody>
+                 <tr>{this.renderTableHeader()}</tr>
+                 {this.renderTableData()}
+              </tbody>
+           </table>
+        </div>
+     )
+  }
 }
+
+// ReactDOM.render(<Table />, document.getElementById('root'));
