8e0a0fb3a19a0bac02886ac99156c0b69f57e6de
Alan <allen0lee@gmail.com>
2021-01-20 17:36:53 +1100

    test table with fake data

--- src/Table.js
+++ src/Table.js
@@ -1,52 +1,74 @@
 import React from 'react'
 
-export default class Table extends React.Component {
-  constructor(props) {
-     super(props)
-     this.state = {
-        students: [
-           { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-           { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-           { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-           { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-        ]
-     }
+class Table extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      cryptoData: [
+        {"Currency": "xrp", "Price": "7,200.03", "24h": "0.0%", "7d": "0.0%", "1 month": "0.0%", "24h Volume": "0", "Mkt Cap": "0"}
+      ]
+    }
   }
 
   renderTableHeader() {
-     let header = Object.keys(this.state.students[0])
-     return header.map((key, index) => {
-        return <th key={index}>{key.toUpperCase()}</th>
-     })
+    console.log("!!!!!!!!!!!!")
+
+    // const prepData = async () => {
+    //   let cryptos = await fetch("https://nameless-sea-84524.herokuapp.com/crypto_api").then(res => {return res.json()})
+      
+    //   console.log(cryptos)
+    //   let cryptoData = [...this.state.cryptoData]
+  
+    //   cryptos.forEach(crypto => {
+    //     cryptoData.push(crypto)
+    //   })
+  
+    //   this.setState({cryptoData})
+      
+    // } 
+
+    // prepData()
+    // console.log(this.state.cryptoData.length)
+
+    let header = Object.keys(this.state.cryptoData[0])
+    return header.map((key, index) => {
+      return <th key={index}>{key}</th>
+    })
   }
 
   renderTableData() {
-     return this.state.students.map((student, index) => {
-        const { id, name, age, email } = student //destructuring
-        return (
-           <tr key={id}>
-              <td>{id}</td>
-              <td>{name}</td>
-              <td>{age}</td>
-              <td>{email}</td>
-           </tr>
-        )
-     })
+    console.log(this.state.cryptoData[0])
+
+    return this.state.cryptoData.map((crypto, index) => {
+      const {Currency, Price, "24h":TwentyFourHour, "7d":SevenDay, "1 month":OneMonth, "24h Volume":TwentyFourHourVolume, "Mkt Cap":MktCap} = crypto //destructuring
+      return (
+        <tr key={Currency}>
+          <td>{Currency}</td>
+          <td>{Price}</td>
+          <td>{TwentyFourHour}</td>
+          <td>{SevenDay}</td>
+          <td>{OneMonth}</td>
+          <td>{TwentyFourHourVolume}</td>
+          <td>{MktCap}</td>
+        </tr>
+      )
+    })
   }
 
   render() {
-     return (
-        <div>
-           <h1 id='title'>React Dynamic Table</h1>
-           <table id='students'>
-              <tbody>
-                 <tr>{this.renderTableHeader()}</tr>
-                 {this.renderTableData()}
-              </tbody>
-           </table>
-        </div>
-     )
+  
+    return (
+      <div>
+          <h1 id='title'>React Dynamic Table</h1>
+          <table id='cryptoData'>
+            <tbody>
+              <tr>{this.renderTableHeader()}</tr>
+              {this.renderTableData()}
+            </tbody>
+          </table>
+      </div>
+    )
   }
 }
 
-// ReactDOM.render(<Table />, document.getElementById('root'));
+export default Table;
